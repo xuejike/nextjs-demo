0cca2106a0dddfa32f4b692d236fb626e3a6904b
xuejike <245674286@qq.com>
2021-02-03 11:14:47 +0800

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -2,8 +2,13 @@
 
 export default (req, res) => {
   req.socket.setNoDelay(true);
-
-  res.statusCode = 200;
+ res.writeHead(200, {
+    'Content-Type': 'text/event-stream',
+    'Cache-Control': 'no-cache, no-transform',
+    'Connection': 'keep-alive'
+  });
+//   res.statusCode = 200;
+  res.write(':ok\n\n');
   res.write(':ok\n\n');
   res.end();
 }
